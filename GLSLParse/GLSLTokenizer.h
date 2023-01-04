83ab15942c0a85046424aa8a142a1b4983060a0c
HuYingzhuo(hugo/hyzboy) <hyzboy@outlook.com>
2023-01-04 20:21:12 +0800

    added GLSLParse's codes.

---
+++ GLSLParse/GLSLTokenizer.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include"GLSLToken.h"
+
+namespace shader_lib
+{
+	class GLSLTokenizer
+	{
+        const char *source;
+        int source_length;
+
+        GLSLToken token_type;
+        int token_length;
+
+	private:
+
+		int ParseToken();
+
+        bool IsWhiteSpace();
+        bool IsLine();
+        bool IsComment();
+        bool IsConstant();
+        bool IsKeyWord();
+        bool IsIdentifier();
+
+	public:
+
+        GLSLTokenizer(const char *,int);
+
+        const int GetLeftLength()const{return source_length;}                   ///<取得剩余代码总长度
+
+		const char *GetToken(GLSLToken *,int *);
+	};//class GLSLTokenizer
+}//namespace shader_lib
